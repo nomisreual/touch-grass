d33f81669929e91c4f171f1da753c5ffcf4ad972
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-07-03 12:37:23 +0200

    fix: decryption now works
    django bytefields may not return data in byte format, forced it

--- client/models.py
+++ client/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from cryptography.fernet import Fernet
 import os
 from django.contrib.auth.models import User
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Client(models.Model):
@@ -17,7 +20,9 @@ class Client(models.Model):
         if not encryption_key:
             raise ValueError("ENCRYPTION_KEY environment variable is not set")
         cipher_suite = Fernet(encryption_key)
-        return cipher_suite.decrypt(self.encrypted_api_key).decode()
+        # Make sure the encrypted key is in bytes:
+        encrypted_key = bytes(self.encrypted_api_key)
+        return cipher_suite.decrypt(encrypted_key).decode()
 
     @key.setter
     def key(self, value) -> None:
--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from client.models import Client
+import os
 
 
 def index(request):
-    return render(request, "main/index.html")
+    # NOTE: to be removed, just for testing it out!
+    client = Client.objects.get(user=request.user)
+    context = {
+        "api_key": client.key,
+    }
+    return render(request, "main/index.html", context=context)
