5707af932d671c1eaab016b360efe4afb4d9dea3
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-07-04 15:57:13 +0200

    test: test cases for Post model

--- main/tests.py
+++ main/tests.py
@@ -1,1 +1,20 @@
-# Create your tests here.
+from django.test import TestCase
+
+from main.models import Post
+
+
+class TestPost(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(
+            title="Test Post",
+            content="Test Content",
+        )
+
+    def test_post_str(self):
+        self.assertEqual(str(self.post), "Test Post")
+
+    def test_post_content(self):
+        self.assertEqual(self.post.content, "Test Content")
+
+    def test_post_title(self):
+        self.assertEqual(self.post.title, "Test Post")
