3ad51dd8c18c0b605502492e1d2bfc632b0d8e6a
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-07-05 18:47:51 +0200

    fix: csrf settings

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ if (os.getenv("DEBUG") == "True") or (os.getenv("GITHUB_WORKFLOW") is not None):
 else:
     DEBUG = False
 
+# CSRF
+CSRF_COOKIE_SECURE = not DEBUG
+CSRF_USE_SESSIONS = not DEBUG
+
 # Read the allowed hosts from the environment variable and split into a list
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
